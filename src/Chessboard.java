a9ef3dba0474ef92f4b1bd593c6cf16eed4355ee
Chayut <chayut.191@gmail.com>
2017-11-15 13:01:29 +0700

    Add drawing function

--- src/Chessboard.java
+++ src/Chessboard.java
@@ -1,12 +1,65 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
-public class Chessboard {
+public class Chessboard extends JFrame {
     Chessboard (int width, int height) {
-        JFrame board = new JFrame("chessboard");
-        board.setBounds(100, 100, width, height);
-        board.setCursor(new Cursor(Cursor.CROSSHAIR_CURSOR));
+        this.setTitle("chessboard");
+        this.setBounds(100, 100, width, height);
+        this.setCursor(new Cursor(Cursor.CROSSHAIR_CURSOR));
 
-        board.setVisible(true);
+        this.setVisible(true);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        int w = this.getWidth(), h = this.getHeight();
+        int boxW = w / 10, boxH = h / 10;
+
+        int currentX = 0, currentY = 0, row = 1;
+
+        Font myFont = new Font("Courier New", Font.BOLD, (w / 40 + h / 40) / 2);
+        g.setFont(myFont);
+
+        for (int j = 0; j < 64; j++) {
+
+            if (j % 8 == 0) {
+                currentY += boxH;
+                currentX = boxW;
+                row += 1;
+            }
+
+            if (row % 2 == 0) {
+                if (j % 2 == 0) {
+                    g.setColor(Color.WHITE);
+                } else {
+                    g.setColor(getRandColor());
+                }
+            } else {
+                if (j % 2 == 0) {
+                    g.setColor(getRandColor());
+                } else {
+                    g.setColor(Color.WHITE);
+                }
+            }
+
+            g.fillRect(currentX, currentY, boxW, boxH);
+            g.setColor(Color.BLACK);
+            g.drawString(String.valueOf(j + 1), currentX + ((boxW / 2) - (boxW / 10)), currentY + (boxH / 2) + (boxH / 20));
+            currentX += boxW;
+        }
+
+        g.setColor(Color.BLACK);
+        g.drawRect(w / 10, h / 10, boxW * 8, boxH * 8);
+
+    }
+
+    private Color getRandColor() {
+        Random rand = new Random();
+        int red = rand.nextInt(55) + 200;
+        int green = rand.nextInt(55) + 200;
+        int blue = rand.nextInt(55) + 200;
+        return new Color(red, green, blue);
     }
 }
