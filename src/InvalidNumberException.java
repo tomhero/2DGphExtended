fb28ff7dce25294952fe8041224fa035b64b6648
Chayut <chayut.191@gmail.com>
2017-11-16 15:52:11 +0700

    add exception and Collections code

---
+++ src/InvalidNumberException.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Chayut on 16/11/2560.
+ *
+ */
+public class InvalidNumberException extends Exception{
+
+    InvalidNumberException(String msg) {
+        super(msg);
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
 
@@ -37,9 +40,27 @@ public class Main {
         createBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                int width = Integer.parseInt(wInput.getText());
-                int height = Integer.parseInt(hInput.getText());
-                new Chessboard(width, height);
+                try {
+                    int width = Integer.parseInt(wInput.getText());
+                    int height = Integer.parseInt(hInput.getText());
+                    if (width < 500 || height < 500) {
+                        throw new InvalidNumberException("please input number more than 500");
+                    }
+                    new Chessboard(width, height);
+                } catch (NumberFormatException numberErr) {
+                    numberErr.printStackTrace();
+                    JOptionPane.showMessageDialog(null,
+                            "Number only please.",
+                            "Number Error",
+                            JOptionPane.ERROR_MESSAGE);
+
+                } catch (InvalidNumberException invalidErr) {
+                    invalidErr.printStackTrace();
+                    JOptionPane.showMessageDialog(window,
+                            "Enter more than 500 please",
+                            "Number Invalid",
+                            JOptionPane.WARNING_MESSAGE);
+                }
             }
         });
 
@@ -49,5 +70,57 @@ public class Main {
         window.add(hInput);
         window.add(createBtn);
         window.setVisible(true);
+
+        // listTest();
+    }
+
+    private static void listTest() {
+        ArrayList<Integer> intList = genIntList();
+        ArrayList<String> stringList = genStringList();
+        System.out.println("origin -> " + intList);
+        System.out.println("origin -> " + stringList + "\n");
+
+        // normal sort asc
+        Collections.sort(intList);
+        System.out.println("sort asc -> " + intList);
+
+        // sort desc
+        Collections.sort(intList, Collections.reverseOrder());
+        System.out.println("sort desc -> " + intList + "\n");
+
+        // remove item
+        stringList.remove("three");
+        System.out.println("remove item ('three') -> " + stringList);
+
+        // reverse item
+        Collections.reverse(stringList);
+        System.out.println("reverse item -> " + stringList);
+
+        // shuffle list
+        Collections.shuffle(stringList);
+        System.out.println("shuffle list -> " + stringList);
+
+        // find max min
+        System.out.println("max: " + Collections.max(intList));
+        System.out.println("min: " + Collections.min(intList));
+
+        // replace all
+        Collections.replaceAll(stringList, "one", "uno");
+        System.out.println("replace all -> " + stringList);
+
+        // rotate list
+        Collections.rotate(stringList, 4);
+        System.out.println("rotate 4 round -> " + stringList);
+
+        // searching
+        System.out.println("index of 'ono' -> " + stringList.indexOf("uno"));
+    }
+
+    private static ArrayList<String> genStringList() {
+        return new ArrayList<>(Arrays.asList("one", "two", "three", "four", "five", "one", "five"));
+    }
+
+    private static ArrayList<Integer> genIntList() {
+        return new ArrayList<>(Arrays.asList(5,6,4,8,5,4,2,1,9));
     }
 }
